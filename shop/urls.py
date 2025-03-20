da9f2ffdbd1a22a194dbf9d7d77ba1ee0cc1f0a5
Komronbek <kxusniddinov25@gmail.com>
2025-03-20 17:49:43 +0500

    initials

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
 urlpatterns = [
-    path('', )
+    path('', index, name='index'),
+    path('home_page/', home_page, name='home_page'),
+    path('order_page/', order_page, name='order_page'),
+    path('order/', main_page, name='main_order'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,23 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Category, Customer, Product, Order, OrderProduct
+from . import services
+from .forms import CustomerForm, OrderProductForm, OrderForm
+from config.settings import MEDIA_ROOT
+from .services import get_product_by_id, get_user_by_phone, get_orderproduct_by_id
+
+
+def home_page(request):
+    if request.GET:
+        product = get_product_by_id(request.GET.get("product_id", 0))
+        return JsonResponse(product)
+
+
+def order_page(request):
+    if request.GET:
+        user = get_user_by_phone(request.GET.get("phone_number", 0))
+        return JsonResponse(user)
+
 
